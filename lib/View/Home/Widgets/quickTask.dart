f9ad5e1c62524f9f74efc1472ae3737dc68987aa
Filipe Ilunga <filipeilunga@gmail.com>
2021-03-18 16:40:39 -0300

    Add QuickTask Card

---
+++ lib/View/Home/Widgets/quickTask.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class QuickTask extends StatelessWidget {
+  QuickTask();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 15, right: 15),
+      height: 120,
+      width: 330,
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 3,
+              color: Colors.black,
+              offset: Offset(2, 2),
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.add_circle,
+            color: Colors.white,
+            size: 50,
+          ),
+          Text(
+            'Quick Task',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 15,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
