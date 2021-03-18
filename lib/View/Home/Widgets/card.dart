7d4575076a5cfa7ad39a1d31cfb5a9b1ee18c1b3
Filipe Ilunga <filipeilunga@gmail.com>
2021-03-18 16:36:47 -0300

    Add Card

---
+++ lib/View/Home/Widgets/card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class CardTask extends StatelessWidget {
+  CardTask();
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: ShapeBorderClipper(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+      ),
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 10,
+          left: 15,
+          bottom: 10,
+        ),
+        width: 330.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left: BorderSide(color: Colors.amber, width: 7.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'My Second Flutter App in Progress',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 15,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 20),
+              child: Text(
+                'Believe it can be done. When you believe something can be done,'
+                'really believe, your mind will find the ways to do it. Believing a'
+                'solution paves the way to solution.',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person_outline_rounded,
+                  color: Colors.grey,
+                  size: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: Text(
+                    'Filipe Ilunga',
+                    style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.calendar_today,
+                  color: Colors.grey,
+                  size: 16,
+                ),
+                Text(
+                  '16.03.2021',
+                  style: TextStyle(
+                    fontSize: 11,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
