baff2942d44ade4ad836e75847c974f03a3f403b
Filipe Ilunga <filipeilunga@gmail.com>
2021-04-02 12:16:28 -0300

    ThemeBuild created

---
+++ lib/themeChanger.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ThemeBuilder extends StatefulWidget {
+  final Widget Function(BuildContext context, Brightness brightness) builder;
+
+  ThemeBuilder(this.builder);
+
+  @override
+  _ThemeBuilderState createState() => _ThemeBuilderState();
+}
+
+class _ThemeBuilderState extends State<ThemeBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
