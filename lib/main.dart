63d0864a9be4f4c876656a5a88e3d7731d853122
Filipe Ilunga <filipeilunga@gmail.com>
2021-04-02 22:26:21 -0300

    Fix theme bug

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ThemeBuilder(builder: (context, _brightness) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Task Manager App',
-        theme: ThemeData(
-          fontFamily: 'San Francisco',
-          primaryColor: Color(0xFF024ACE),
-          primaryColorDark: Color(0xFF024ACE),
-          brightness: Brightness.light,
-          accentColor: Colors.white,
-          backgroundColor: Colors.blue[100],
-          appBarTheme: AppBarTheme(
-              backgroundColor: Color(0xFF024ACE),
-              centerTitle: true,
-              iconTheme: IconThemeData(
-                color: Colors.white,
-              )),
-          accentTextTheme: TextTheme(
-            headline6: TextStyle(
-              fontWeight: FontWeight.w600,
-            ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager App',
+      theme: ThemeData(
+        fontFamily: 'San Francisco',
+        primaryColor: Color(0xFF024ACE),
+        primaryColorDark: Color(0xFF024ACE),
+        brightness: Brightness.light,
+        accentColor: Colors.white,
+        backgroundColor: Colors.blue[100],
+        appBarTheme: AppBarTheme(
+            backgroundColor: Color(0xFF024ACE),
+            centerTitle: true,
+            iconTheme: IconThemeData(
+              color: Colors.white,
+            )),
+        accentTextTheme: TextTheme(
+          headline6: TextStyle(
+            fontWeight: FontWeight.w600,
           ),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Home(),
-      );
-    });
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
   }
 }
