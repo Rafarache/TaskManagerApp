b09c663b7fffd4b66da9e53c806a068c6a0e7953
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-18 15:22:36 -0300

    Add sharedPreferences

---
+++ lib/Model/userPreferences.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences {
+  static final UserPreferences _instance = UserPreferences._ctor();
+  factory UserPreferences() {
+    return _instance;
+  }
+
+  UserPreferences._ctor();
+
+  SharedPreferences _prefs;
+
+  init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  get data {
+    return _prefs.getInt('data') ?? 1;
+  }
+
+  set data(int value) {
+    _prefs.setInt('data', value);
+  }
+}
