63a37888f7a7b19f24d31f7cb107ddb8b6dd24f6
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-18 13:29:22 -0300

    Remove taskDone listView

--- lib/View/Home/HomePage.dart
+++ lib/View/Home/HomePage.dart
@@ -102,255 +102,46 @@ class _HomeState extends State<Home> {
                       Padding(
                           padding: const EdgeInsets.only(left: 25.0),
                           child: TextButton(
-                            style: TextButton.styleFrom(
-                                textStyle: TextStyle(
-                              fontWeight:
-                                  _menuIndex == 1 ? FontWeight.bold : null,
-                            )),
-                            onPressed: () {
-                              setState(() {
-                                _menuIndex = 1;
-                              });
-                            },
-                            child: Text(
-                              "Fazendo(${tasks.length + tasksPinned.length})",
-                            ),
-                          )),
+                              style: TextButton.styleFrom(
+                                  textStyle: TextStyle(
+                                fontWeight:
+                                    _menuIndex == 1 ? FontWeight.bold : null,
+                              )),
+                              onPressed: () {
+                                setState(() {
+                                  _menuIndex = 1;
+                                });
+                              },
+                              child: Text(
+                                  "Fazendo(${tasks.length + tasksPinned.length})"))),
                       Padding(
                           padding: const EdgeInsets.only(left: 25.0),
                           child: TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: TextStyle(
+                              style: TextButton.styleFrom(
+                                  textStyle: TextStyle(
                                 fontWeight:
                                     _menuIndex == 0 ? FontWeight.bold : null,
-                              ),
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _menuIndex = 0;
-                              });
-                            },
-                            child: Text(
-                              "Feitos(${tasksDone.length})",
-                            ),
-                          )),
+                              )),
+                              onPressed: () {
+                                setState(() {
+                                  _menuIndex = 0;
+                                });
+                              },
+                              child: Text("Feitos(${tasksDone.length})"))),
                     ],
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(right: 15.0),
-                  child: IconButton(icon: Icon(Icons.menu), onPressed: null),
-                ),
+                    padding: const EdgeInsets.only(right: 15.0),
+                    child: IconButton(icon: Icon(Icons.menu), onPressed: null)),
               ],
             ),
             SizedBox(height: 30),
             _menuIndex == 1
                 ? TaskCard(
                     helper, tasks, _getAllTasks, _getAllTasksDone, _showTask)
-                : ListView.builder(
-                    primary: false,
-                    shrinkWrap: true,
-                    itemCount: tasksDone.length,
-                    itemBuilder: (context, index) {
-                      DateFormat format = DateFormat("d MM y");
-                      var dateUpdate = format.parse(tasksDone[index].due);
-                      tasksDone[index].diference =
-                          (dateUpdate.difference(DateTime.now()).inHours / 24)
-                              .round();
-
-                      return Container(
-                        margin: const EdgeInsets.only(
-                            left: 20, right: 20, bottom: 10),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _cardTapDone = index;
-                              _cardBoolDone = !_cardBoolDone;
-                            });
-                          },
-                          child: Dismissible(
-                            key: Key(DateTime.now()
-                                .millisecondsSinceEpoch
-                                .toString()),
-                            direction: DismissDirection.horizontal,
-                            background: Container(
-                              color: Colors.red,
-                              child: Align(
-                                alignment: Alignment(-0.9, 0),
-                                child: Icon(Icons.delete, color: Colors.white),
-                              ),
-                            ),
-                            onDismissed: (direction) {
-                              setState(() {
-                                _cardTapDone = -1;
-                                _lastRemoved = tasksDone[index];
-                                helper.deleTask(tasksDone[index].id);
-                                tasksDone.removeAt(index);
-                                _lastRemovedPos = index;
-                              });
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  content: Row(
-                                    children: [
-                                      Icon(Icons.warning, color: Colors.yellow),
-                                      Expanded(
-                                        child: RichText(
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 3,
-                                            text: TextSpan(
-                                                style: TextStyle(
-                                                  fontSize: 14,
-                                                ),
-                                                children: [
-                                                  TextSpan(text: "A tarefa "),
-                                                  TextSpan(
-                                                      text:
-                                                          "\"${_lastRemoved.title}\"",
-                                                      style: TextStyle(
-                                                          fontWeight:
-                                                              FontWeight.bold)),
-                                                  TextSpan(
-                                                      text: " foi removida!"),
-                                                ])),
-                                      ),
-                                    ],
-                                  ),
-                                  action: SnackBarAction(
-                                      textColor: Colors.white,
-                                      label: 'Desfazer',
-                                      onPressed: () {
-                                        setState(() {
-                                          tasksDone.insert(
-                                              _lastRemovedPos, _lastRemoved);
-                                          helper.saveTask(_lastRemoved);
-                                        });
-                                      }),
-                                  duration: Duration(seconds: 2),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.only(right: 10),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Theme.of(context).cardColor),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.only(
-                                      top: 10.0,
-                                      left: 15.0,
-                                      bottom: 10.0,
-                                      right: 12.0,
-                                    ),
-                                    width: MediaQuery.of(context).size.width,
-                                    child: Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Container(
-                                          margin:
-                                              const EdgeInsets.only(right: 10),
-                                          padding: const EdgeInsets.all(10),
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            color: Colors.green,
-                                          ),
-                                          child: Icon(
-                                            Icons.done,
-                                            color: Colors.white,
-                                            size: 30,
-                                          ),
-                                        ),
-                                        Flexible(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                tasksDone[index]
-                                                    .title, // TITULO -------------------------------------
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w700,
-                                                  fontSize: 15,
-                                                ),
-                                                maxLines: (_cardTap == index) &&
-                                                        (_cardBool == true)
-                                                    ? 2
-                                                    : 1,
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                              Padding(
-                                                padding: const EdgeInsets.only(
-                                                    top: 8.0, bottom: 8),
-                                                child: Text(
-                                                  tasksDone[index]
-                                                      .subject, // DESCRIÇÃO --------------------------------------------
-                                                  style: TextStyle(
-                                                    color: Colors.grey,
-                                                    fontSize: 13,
-                                                  ),
-                                                  maxLines:
-                                                      (_cardTapDone == index) &&
-                                                              (_cardBoolDone ==
-                                                                  true)
-                                                          ? 10
-                                                          : 1,
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
-                                                ),
-                                              ),
-                                              Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: [
-                                                  SizedBox(),
-                                                  Container(
-                                                    child: Row(
-                                                      children: [
-                                                        Icon(
-                                                          Icons.calendar_today,
-                                                          color: Colors.grey,
-                                                          size: 16,
-                                                        ),
-                                                        Padding(
-                                                          padding:
-                                                              const EdgeInsets
-                                                                      .only(
-                                                                  left: 8.0),
-                                                          child: Text(
-                                                            tasksDone[index]
-                                                                .due,
-                                                            style: TextStyle(
-                                                              fontSize: 11,
-                                                              color:
-                                                                  Colors.grey,
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      ],
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                : TaskCard(helper, tasksDone, _getAllTasks, _getAllTasksDone,
+                    _showTask),
           ],
         ),
       ),
