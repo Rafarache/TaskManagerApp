62abc5863ecfd67691e7449f040823d4c1adb618
filipeilunga <filipeilunga@gmail.com>
2021-07-16 20:56:57 -0300

    Add empty image 2

--- lib/Assets/image/Image.dart
+++ lib/Assets/image/Image.dart
@@ -1,7 +1,6 @@
 class Assets{
   static final emptyState = "lib/Assets/image/EmptyState.png";
-static final calendar = "lib/Assets/image/calendar.png";
-static final darkCalendar = "lib/Assets/image/darkCalendar.jpg";
-static final lightCalendar ='lib/Assets/image/lightCalendar.jpg';
+    static final emptyState2 = "lib/Assets/image/EmptyState2.png";
+
    
 }
