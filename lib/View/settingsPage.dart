0e5a0889a4a719a3719777b789f7d3c11905ff44
filipeilunga <filipeilunga@gmail.com>
2021-07-18 18:46:20 -0300

    Remove imports

--- lib/View/settingsPage.dart
+++ lib/View/settingsPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icofont_flutter/icofont_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:taskmanager/blocs/theme.dart';
+
 
 class SettingsPage extends StatefulWidget {
   SettingsPage();
